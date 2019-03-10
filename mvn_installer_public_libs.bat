4e88e78a9ae7edb747a621ee07ca4462a28ed576
Boopathi Sakthivel <boopathisakthivel.in@gmail.com>
2019-03-11 00:11:34 +0530

    initial commit

---
+++ mvn_installer_public_libs.bat
@@ -0,0 +1,2 @@
+start mvn install:install-file -Dfile=iText-5.0.5.jar -DgroupId=blabs.lib -DartifactId=iText-5.0.5 -Dversion=0.1 -Dpackaging=jar -DgeneratePom=true
+start mvn install:install-file -Dfile=iText-5.0.5.jar -DgroupId=blabs.lib -DartifactId=iText-5.0.5 -Dversion=0.1 -Dpackaging=jar -DgeneratePom=true
